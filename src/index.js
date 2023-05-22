dfe7976e7317baf6c16c5b9e75e76310516cb18e
Vlad <kobra6794@yandex.by>
2023-05-22 17:23:55 +0300

    Считана информация с файла + представлена в виде таблицы

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import "./main.scss";
 import "./fonts/fonts.scss";
+const data = require("./static/data.json");
+
+const informationBLock = document.querySelector(".lab1__information");
+let elem = document.createElement("table");
+let dataArray = [];
+
+for (let i = 0; i < data.namespace.length; i++) {
+  dataArray.push(data.namespace[i]);
+}
+elem.innerHTML += `
+        <tr>
+            <th class="th-bold">№ по п/п</th>
+            <th class="th-bold">Наименование</th>
+            <th class="th-bold">Категория</th>
+            <th class="th-bold">Цена</th>
+        </tr>
+`;
+for (let i = 0; i < dataArray.length; i++) {
+  elem.innerHTML += `
+    <tr>
+        <th>${dataArray[i].id + 1}</th>
+        <th>${dataArray[i].name}</th>
+        <th>${dataArray[i].category}</th>
+        <th>${dataArray[i].cost}</th>
+    <tr/>
+    `;
+}
+informationBLock.append(elem);
