d648cfbaac9ffa1e220a04495f5260624bd30f82
Vlad <kobra6794@yandex.by>
2023-05-23 03:11:01 +0300

    Завершено написание скриптов

--- src/index.js
+++ src/index.js
@@ -16,6 +16,19 @@ for (let i = 0; i < dataArray.length; i++) {
   arrayCount.push(dataArray[i].count);
 }
 
+let arrayOfCategory = [];
+
+for (let i = 0; i < dataArray.length; i++) {
+  let obj = {};
+  let id = { id: dataArray[i].id };
+  let category = { category: dataArray[i].category };
+  obj = Object.assign(id, category);
+  arrayOfCategory.push(obj);
+}
+console.log(arrayOfCategory);
+
+let mergedArray = [];
+
 const tableHeader = `
         <tr>
             <th class="th-bold">№ по п/п</th>
@@ -48,6 +61,7 @@ sortBtn.addEventListener("click", () => {
   if (sortSelection.value === "Сортировка выбором") {
     selectionSort(arrayCount);
   } else if (sortSelection.value === "Сортировка слиянием") {
+    mergeSort(arrayOfCategory);
   }
   console.log(sortSelection.value);
 });
@@ -153,4 +167,112 @@ function selectionSort(inputArr) {
             `;
   }
   informationBLock.append(elem);
+}
+
+function merge(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = merge(arr.slice(0, mid));
+  let right = merge(arr.slice(mid));
+  function mergeSort(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i].category < arr2[j].category) {
+        result.push(arr1[i]);
+        i++;
+      } else {
+        result.push(arr2[j]);
+        j++;
+      }
+    }
+    while (i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    }
+    while (j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+    return result;
+  }
+  return mergeSort(left, right);
+}
+
+function mergeSort(array) {
+  array = merge(array);
+  console.log(array);
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < dataArray.length; j++) {
+      if (
+        array[i].category === dataArray[j].category &&
+        array[i].id === dataArray[j].id
+      ) {
+        mergedArray.push(dataArray[j]);
+      }
+    }
+  }
+  let clothes = mergedown(
+    mergedArray.filter((item) => item.category == "Одежда")
+  );
+  let products = mergedown(
+    mergedArray.filter((item) => item.category == "Продукты")
+  );
+  let proms = mergedown(
+    mergedArray.filter((item) => item.category == "Промтовары")
+  );
+  let elect = mergedown(
+    mergedArray.filter((item) => item.category == "Электроника")
+  );
+  let newArr = clothes.concat(products).concat(proms).concat(elect);
+  console.log(proms);
+  console.log(newArr);
+
+  elem.innerHTML = "";
+  elem.innerHTML += tableHeader;
+  for (let i = 0; i < mergedArray.length; i++) {
+    elem.innerHTML += `
+              <tr>
+                  <th>${i + 1}</th>
+                  <th>${newArr[i].name}</th>
+                  <th>${newArr[i].category}</th>
+                  <th>${newArr[i].cost}</th>
+                  <th>${newArr[i].count}</th>
+              <tr/>
+              `;
+  }
+  informationBLock.append(elem);
+}
+
+function mergedown(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = merge(arr.slice(0, mid));
+  let right = merge(arr.slice(mid));
+  function mergeSort(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i].cost > arr2[j].cost) {
+        result.push(arr1[i]);
+        i++;
+      } else {
+        result.push(arr2[j]);
+        j++;
+      }
+    }
+    while (i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    }
+    while (j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+    return result;
+  }
+  return mergeSort(left, right);
 }
